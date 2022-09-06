2ebc3cfa3d0e209b64e48f6d575431880fa2d702
VENKAT PRASAD DANDSENA <venkatprasaddandsena@gmail.com>
2022-09-07 02:14:25 +0530

    ready for deployment

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+app=Flask(__name__)
+#load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values)).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values)).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output)
+    return jsonify(output[0])
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    input=scaler.transform(np.array(data).reshape(1,-1))
+    output=regmodel.predict(input)[0]
+    return render_template('home.html',prediction_text=f"The price of the house is {output} with accuracy of 68.40%")
+
+if __name__=="__main__":
+    app.run(debug=True)
